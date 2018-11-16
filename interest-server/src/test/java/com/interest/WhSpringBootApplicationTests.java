ef4da1e68a39d3a67937fecc09d2b7d7b56de9cd
erintony <VanessaYinmd@163.com>
2018-11-16 16:44:17 +0800

    ignore yml
    ignore .yml file

--- interest-server/src/test/java/com/interest/WhSpringBootApplicationTests.java
+++ interest-server/src/test/java/com/interest/WhSpringBootApplicationTests.java
@@ -2,37 +2,11 @@ package com.interest;
 
 import org.junit.Test;
 
-import com.interest.utils.DateUtil;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-
 public class WhSpringBootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		System.out.println(new BCryptPasswordEncoder().encode("wanghuan0105"));
-	}
+    @Test
+    public void test(){
 
-	public class Person{
-		public void a(){
-			System.out.println("这是人类");
-		}
-	}
-
-	public class Chainese extends Person{
-		public void b(){
-			super.a();
-			this.c();
-		}
-
-		public void c(){
-			System.out.println("这是中国人");
-		}
-	}
-
-	@Test
-	public void test(){
-		new Chainese().b();
-	}
+    }
 
 }
