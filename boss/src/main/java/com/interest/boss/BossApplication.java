f551b024c046fbf77c369b8fc188c6073a2c0cca
bob <jiabo@live.com>
2019-02-21 17:55:42 +0800

    add boss

---
+++ boss/src/main/java/com/interest/boss/BossApplication.java
@@ -0,0 +1,13 @@
+package com.interest.boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BossApplication.class, args);
+    }
+
+}
