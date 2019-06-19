fc1260a1b14e127698e672a87cba560bf39436e5
smallsnail-wh <1147462829@qq.com>
2019-06-19 10:59:28 +0800

    add other configuration

--- interest-server/src/main/java/com/interest/config/OtherConfiguration.java
+++ interest-server/src/main/java/com/interest/config/OtherConfiguration.java
@@ -1,5 +1,7 @@
 package com.interest.config;
 
+import com.interest.properties.OtherProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,14 +11,17 @@ import javax.servlet.MultipartConfigElement;
 @Configuration
 public class OtherConfiguration {
 
+    @Autowired
+    private OtherProperties otherProperties;
+
     /**
      * 文件上传临时路径
      */
-//    @Bean
-//    public MultipartConfigElement multipartConfigElement() {
-//        MultipartConfigFactory factory = new MultipartConfigFactory();
-//        factory.setLocation("/interest/file/tmp");
-//        return factory.createMultipartConfig();
-//    }
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(otherProperties.getTemporaryPath());
+        return factory.createMultipartConfig();
+    }
 
 }
